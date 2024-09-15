78a083b5cc907a598f96d708719bdc79e88b9f11
Samjhana Neupane <samjhanaa.neupane@gmail.com>
2024-09-15 15:25:03 +0545

    Added edit functionality

--- script.js
+++ script.js
@@ -2,10 +2,28 @@ const input = document.getElementById("todo-input");
 const btnInput = document.getElementById("add-input");
 const todoList = document.getElementById("todo-list");
 
-function listDisplay() {
+function addTask() {
     const makeList = document.createElement("li");
     makeList.textContent = input.value;
+
+    const editTask = document.createElement("Button")
+    editTask.textContent = "Edit";
+    editTask.className = "edit-button";
+
+    editTask.onclick = function edit() {
+        const newEditTask = prompt("Edit", input)
+        makeList.firstchild.textContent = newEditTask.trim()
+    }
+
     todoList.appendChild(makeList);
+    todoList.appendChild(editTask);
+
     input.value = "";
 
+
+
 }
+
+
+
+
