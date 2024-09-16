7a19ca7986793996523cbf2f6633223fa89fbe8a
Samjhana Neupane <samjhanaa.neupane@gmail.com>
2024-09-16 23:42:26 +0545

    added delete functionality

--- script.js
+++ script.js
@@ -1,27 +1,50 @@
+
 const input = document.getElementById("todo-input");
 const btnInput = document.getElementById("add-input");
 const todoList = document.getElementById("todo-list");
 
+
 function addTask() {
+    //create a div to add list items, edit and delete button
+    const taskContainer = document.createElement("div");
+    taskContainer.className = "task-container";
+
+    //create space to add list item
     const makeList = document.createElement("li");
     makeList.textContent = input.value;
 
+    //button to edit task
     const editTask = document.createElement("Button")
     editTask.textContent = "Edit";
     editTask.className = "edit-button";
 
+    //button to delete task
+    const deleteTask = document.createElement("Button")
+    deleteTask.textContent = "Delete";
+    deleteTask.className = "delete-button";
+
+    //function to edit 
     editTask.onclick = function edit() {
-        const newEditTask = prompt("Edit", input)
-        makeList.firstchild.textContent = newEditTask.trim()
+        const newEditTask = prompt("Edit", makeList.textContent)
+        makeList.firstChild.textContent = newEditTask.trim()
     }
 
-    todoList.appendChild(makeList);
-    todoList.appendChild(editTask);
+    //function to delete
+    deleteTask.onclick = function () {
+        taskContainer.remove();
+    }
 
+    //add list , edit and delete to task container 
+    taskContainer.appendChild(makeList);
+    taskContainer.appendChild(editTask);
+    taskContainer.appendChild(deleteTask);
+
+    // add task to display in todo list
+    todoList.appendChild(taskContainer);
+
+
+    //clear input space
     input.value = "";
-
-
-
 }
 
 
